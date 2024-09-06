802cf5390faf93e21ea3e7599b20a9e10c73e34b
TalytaBP <talyytta@gmail.com>
2024-09-06 11:59:05 -0300

    Adição imagem clicada

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.calculadora
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,10 +20,15 @@ class MainActivity : AppCompatActivity() {
         var segundoNumero:TextInputEditText = findViewById(R.id.editSegundoNum)
         var btnSomar: Button = findViewById(R.id.btnSomar)
         var txtResultado: TextView = findViewById(R.id.txtResultado)
+        var imgCalc: ImageView = findViewById(R.id.imgCalc)
 
         btnSomar.setOnClickListener{
            var resultado = primeiroNumero.text.toString().toDouble() + segundoNumero.text.toString().toDouble()
             txtResultado.setText("Resultado da soma: ${resultado}")
+        }
+
+        imgCalc.setOnClickListener{
+            txtResultado.setText("Imagem clicada")
 
         }
     }
